7fc459f16dd628733c683ffd642374b525a702bd
Parikshit Hooda <phooda804@live.com>
2021-10-07 17:07:38 +0530

    example: chain fn1 fn2 fn3

--- callbacks_2.js
+++ callbacks_2.js
@@ -1,15 +1,17 @@
 //try to make a complex callback function chain
 
-function fn1(index, callback1){
+function fn1(index, callback){
   if (index %2 == 0) { //even index
     var jsonObj = {};
   fetch(`https://jsonplaceholder.typicode.com/todos/${index}`)
   .then(response => response.json())
   .then(json => {
     jsonObj = json;
-    console.log(jsonObj);
+    callback(jsonObj, "todos");
+
   });
-  setTimeout(function(){callback1(jsonObj, "todos");}, 5000);
+  // console.log("logging jsonOBj" + JSON.stringify(jsonObj));
+
 
   } else { // odd index
     var jsonObj = {};
@@ -18,26 +20,42 @@ function fn1(index, callback1){
     .then(json => {
       jsonObj = json;
       console.log(jsonObj);
-    });
+      callback(jsonObj, "posts");
 
-    setTimeout(function(){callback1(jsonObj, "posts");}, 10000);
+    });
+    // console.log("logging jsonOBj" + JSON.stringify(jsonObj));
 
   }
 }
 
-function fn2(jsonObj, item){
-  console.log("fn2: " + item );
+function fn2(jsonObj, item, callback){
+  // console.log("fn2: " + item );
+  if(item === "posts") {
+    console.log("fn2: posts");
+    console.log("jsonObj title:" + jsonObj.title);
+    callback(`post: ${jsonObj.title}`);
+  } else if (item === "todos") {
+    console.log("fn2: todos");
+    console.log("jsonObj title:" + jsonObj.title);
+    callback(`todo: ${jsonObj.title}`);
+  }
+
 }
 
-function fn3(){
+function fn3(objTitle){
   console.log("fn3");
-
+  console.log("fn3: " + objTitle);
 }
 
 function fn4(){
-
-
+  console.log("fn4");
 }
 
-fn1(1, fn2);
-fn1(2, fn2);
+//fn1 calls fn2 calls fn3 calls fn4
+
+fn1(1, function(jsonObj, item){
+  console.log("jsonObj in fn1: " + JSON.stringify(jsonObj));
+  fn2(jsonObj, item, function(objTitle){
+    fn3(objTitle);
+  });
+});
